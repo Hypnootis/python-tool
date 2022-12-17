182b9be2aa874a3bdc48416fa645cd17575bc3f5
Hypnootis <tm.pasanen@outlook.com>
2022-12-17 21:43:47 +0200

    Trying to solve file endings, folders are created

--- ex.py
+++ ex.py
@@ -1,14 +1,24 @@
 import os, shutil
-# TODO: Add some tests!
+
 data_path = "data/" # Relative path
 # TODO: entrypoint for program
 
 data_files = os.listdir(data_path)
 # Data should be a pair of jpg/txt files! Maybe add support for other filetypes or remove filetype dependency
-def move_files(files: list): # TODO: Make directory if it doesn't exist...
+def create_folders():
+    try:
+        os.makedirs("images/test")
+        os.makedirs("images/train")
+        os.makedirs("labels/test")
+        os.makedirs("labels/train")
+    except FileExistsError as e:
+        print(e)
+
+def move_files(files: list):
+    copied_files = files # For popping purposes
     if len(files) != 0:
-        for file in files:
-            if f"{file.split('.')[0]}.txt" in files and f"{file.split('.')[0]}.jpg" in files:
+        for file in copied_files:
+            if f"{file.split('.')[0]}.txt" in files:
                 if file.endswith(".jpg"): 
                     shutil.move(f"{data_path}{file}", f"images/")
                 else:
@@ -23,20 +33,18 @@ def move_files(files: list): # TODO: Make directory if it doesn't exist...
 # Labels and images have the same name but different filetype by design
 def split_files(files: list):
     copy_files = files # For getting the split version
-    index = 0
-    validation, train = [], []
     split_index = len(copy_files) * 0.2
     for file in copy_files:
-        copy_file = file.split('.')[0]
+        split_file = file.split('.')[0]
         if copy_files.index(file) < split_index:
-            shutil.move(f"images/{copy_file}.jpg", f"images/test/")
-            shutil.move(f"labels/{copy_file}.txt", f"labels/test/")
+            shutil.move(f"images/{file}", f"images/test/")
+            shutil.move(f"labels/{split_file}.txt", f"labels/test/")
         else:
-            shutil.move(f"images/{copy_file}.jpg", f"images/train/")
-            shutil.move(f"labels/{copy_file}.txt", f"labels/train/")
+            shutil.move(f"images/{file}", f"images/train/")
+            shutil.move(f"labels/{split_file}.txt", f"labels/train/")
     print("Done splitting files")
 
-
+create_folders()
 move_files(data_files)
 
     
